b34d498cadffb931cd17fb67ed0aa5445dda0e46
weilanhzuan <weilanhzuan@163.com>
2015-11-29 23:36:04 +0800

    修改框架

--- Application/Api/Common/function.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/**
-	 * 系统非常规MD5加密方法
-	 * @param  string $str 要加密的字符串
-	 * @return string 
-	 */
-	function parents_md5($str, $key = 'Parents'){
-		return '' === $str ? '' : md5(sha1($str) . $key);
-	}
--- Application/Api/Model/MemberCommentModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Api\Model;
-use Think\Model\ViewModel;
-class MemberCommentModel  extends ViewModel{
-  public $viewFields = array(
-     'Comment'=>array('comment_id','article_id','uid','comment_pid','comment_content','add_time','status','comment_pic'),
-     'Member'=>array('username','photos', '_on'=>'Comment.uid=Member.uid'),
-   ); 
-}
